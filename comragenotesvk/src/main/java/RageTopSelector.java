6f70e916074d80778789f11b10b34a6fc1d112b7
sevoda <sevoda>
2019-03-25 22:07:00 +0400

    Add Selector + selector's test

---
+++ comragenotesvk/src/main/java/RageTopSelector.java
@@ -0,0 +1,22 @@
+import listentities.RageServerEntity;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class RageTopSelecter {
+    public List<RageServerEntity> selectTop(List<RageServerEntity> servers, Comparator<RageServerEntity> comparator, int count) {
+        if(count <= 0) throw new IllegalArgumentException("can not select 0 or less elements");
+
+        PriorityQueue<RageServerEntity> queue = new PriorityQueue<>(comparator);
+        queue.addAll(servers);
+        List<RageServerEntity> top = new ArrayList<>();
+
+        for(int i = 0; i < count; ++i) {
+            top.add(queue.poll());
+        }
+
+        return top;
+    }
+}
